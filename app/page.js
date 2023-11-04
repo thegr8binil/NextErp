f8a82a826c515eec0632ab49246a1289d4d55c50
Binil B <binil.b.re@gmail.com>
2023-11-04 14:16:25 +0530

    features

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import NavBar from "@/components/NavBar"
 import HeroSection from "@/components/HeroSection"
+import Features from "@/components/Features"
 import { Clash } from "./layout"
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center">
     <NavBar/>
     <HeroSection/>
+    <Features/>
     </main>
   )
 }
---
+++ components/Features.js
@@ -0,0 +1,56 @@
+import Modal from "@/components/Modal";
+import { Clash } from "@/app/layout";
+export default function Features() {
+  return (
+    <main className="max-w-12xl relative">
+      <div className="flex flex-col p-5 gap-4  md:flex-row-reverse sm:items-center mt-2 lg:mt-14">
+        <img src="Group.png" alt="hero" className=" md:w-96 lg:w-1/2" />
+        <div className="sm:ml-4">
+          <div className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl">
+            <div>
+              <img src="1.png" alt="hero" className="w-22 mt-1" />
+            </div>
+            <div>
+              <h1 className="text-lg font-semibold">
+                General Ledger Management
+              </h1>
+              <p className="font-normal text-slate-700">
+                Maintain a central repository for recording and tracking
+                financial transactions, enabling accurate and up-to-date
+                reporting.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl mt-3">
+            <div>
+              <img src="2.png" alt="hero" className="w-22 mt-1" />
+            </div>
+            <div>
+              <h1 className="text-lg font-semibold">
+              Accounts Receivable and Payable
+              </h1>
+              <p className="font-normal text-slate-700">
+              Streamline the management of incoming and outgoing payments, track customer invoices, manage credit limits, and automate payment reminders.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl mt-3">
+            <div>
+              <img src="3.png" alt="hero" className="w-22 mt-1" />
+            </div>
+            <div>
+              <h1 className="text-lg font-semibold">
+                General Ledger Management
+              </h1>
+              <p className="font-normal text-slate-700">
+                Maintain a central repository for recording and tracking
+                financial transactions, enabling accurate and up-to-date
+                reporting.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
