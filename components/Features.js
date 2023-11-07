c832d0e9af79d05c8af161a5e9283fed4767c9dd
Binil B <binil.b.re@gmail.com>
2023-11-07 15:55:18 +0530

    animations

--- components/Features.js
+++ components/Features.js
@@ -1,12 +1,100 @@
+"use client";
 import Modal from "@/components/Modal";
 import { Clash } from "@/app/layout";
+import { motion } from "framer-motion";
+const fadeInAnimation = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeInOut",
+    },
+  },
+};
+const fadeInAnimationdelay = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      ease: "easeInOut",
+    },
+  },
+};
+const fadeInAnimationdelay1 = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+const fadeInAnimationdelay2 = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1.2,
+      ease: "easeInOut",
+    },
+  },
+};
+const ScaleinAnimation = {
+  initial: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
 export default function Features() {
   return (
     <main className="max-w-12xl relative">
       <div className="flex flex-col p-5 gap-4 md:flex-row-reverse sm:items-center mt-2 lg:mt-14">
-        <img src="Group.png" alt="hero" className=" md:w-96 lg:w-1/2" />
+        <motion.img
+          src="Group.png"
+          alt="hero"
+          className=" md:w-96 lg:w-1/2"
+          variants={ScaleinAnimation}
+          initial="initial"
+          whileInView="animate"
+          delay={1}
+          viewport={{ once: true }}
+        ></motion.img>
         <div className="sm:ml-4 max-w-2xl">
-          <div className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl">
+          <motion.div
+            className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl"
+            variants={fadeInAnimation}
+            initial="initial"
+            whileInView="animate"
+            delay={1}
+            viewport={{ once: true }}
+          >
             <div>
               <img src="1.png" alt="hero" className="w-40 sm:w-24 mt-1" />
             </div>
@@ -20,8 +108,15 @@ export default function Features() {
                 reporting.
               </p>
             </div>
-          </div>
-          <div className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl mt-3">
+          </motion.div>
+          <motion.div
+            className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl mt-3"
+            variants={fadeInAnimationdelay}
+            initial="initial"
+            whileInView="animate"
+            delay={1}
+            viewport={{ once: true }}
+          >
             <div>
               <img src="2.png" alt="hero" className="w-40 sm:w-28 mt-1" />
             </div>
@@ -35,8 +130,15 @@ export default function Features() {
                 payment reminders.
               </p>
             </div>
-          </div>
-          <div className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl mt-3">
+          </motion.div>
+          <motion.div
+            className="flex gap-3 border-2 border-slate-300 p-5 rounded-3xl mt-3"
+            variants={fadeInAnimationdelay1}
+            initial="initial"
+            whileInView="animate"
+            delay={1}
+            viewport={{ once: true }}
+          >
             <div>
               <img src="3.png" alt="hero" className="w-40 sm:w-20 mt-1" />
             </div>
@@ -47,7 +149,7 @@ export default function Features() {
                 calculations, asset maintenance, and disposal.
               </p>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </main>
--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,38 +1,133 @@
-'use client';
+"use client";
 import Modal from "@/components/Modal";
 import { Clash } from "@/app/layout";
 import { motion } from "framer-motion";
-
-
+const fadeInAnimation = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+const fadeInAnimationdelay = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      ease: "easeInOut",
+    },
+  },
+};
+const fadeInAnimationdelay1 = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+const ScaleinAnimation = {
+  initial: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
 export default function HeroSection() {
   return (
     <main className="max-w-12xl relative">
       <div className="flex flex-col p-5 gap-4 text-2xl font-medium md:flex-row-reverse sm:items-center md:text-3xl mt-2 lg:mt-14 xl:text-3xl 2xl:text-5xl">
-        <img src="hero.png" alt="hero" className=" md:w-96 lg:w-1/2" />
+        <motion.img
+          src="hero.png"
+          alt="hero"
+          className=" md:w-96 lg:w-1/2"
+          variants={fadeInAnimation}
+          initial="initial"
+          whileInView="animate"
+          delay={1}
+          viewport={{ once: true }}
+        />
         <div className="sm:ml-4">
-        
-          <motion.h1 className={`${Clash.className} mt-4 leading-tight`}>
+          <motion.h1
+            className={`${Clash.className} mt-4 leading-tight`}
+            variants={fadeInAnimation}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+          >
             Efficiently Manage Your Business with Customizable Cloud-based
             Accounting ERP
           </motion.h1>
-          <h1 className="text-sm font-normal mt-3 sm:text-md md:text-lg">
+          <motion.h1
+            className="text-sm font-normal mt-3 sm:text-md md:text-lg"
+            variants={fadeInAnimationdelay}
+            initial="initial"
+            whileInView="animate"
+            delay={1}
+            viewport={{ once: true }}
+          >
             Designed to revolutionize the way you manage your business. Our
             customizable software provides you with the tools you need to
             streamline your financial management and optimize your business
             operations, all in one comprehensive platform
-          </h1>
-          <div className="flex items-center justify-start text-lg font-normal mt-2 ">
+          </motion.h1>
+          <motion.div
+            className="flex items-center justify-start text-lg font-normal mt-2 "
+            variants={fadeInAnimationdelay1}
+            initial="initial"
+            whileInView="animate"
+            delay={1}
+            viewport={{ once: true }}
+          >
             <Modal buttonText="Try Free Demo" />
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="flex items-center justify-center flex-col">
-        <img
-          src="Person.png"
-          alt="hero-bg"
-          className="px-10 "
-        />
-        <h1 className={`${Clash.className} text-xl sm:text-3xl mt-4`}>One <span className="bg-orange-100 p-1">Platform</span> For Any Business</h1>
+        <motion.img src="Person.png" alt="hero-bg" className="px-10 "
+         variants={ScaleinAnimation}
+         initial="initial"
+         whileInView="animate"
+         delay={1}
+         viewport={{ once: true }} />
+        <motion.h1
+          className={`${Clash.className} text-xl sm:text-3xl mt-4`}
+          variants={fadeInAnimationdelay1}
+          initial="initial"
+          whileInView="animate"
+          delay={1}
+          viewport={{ once: true }}
+        >
+          One <span className="bg-orange-100 p-1">Platform</span> For Any
+          Business
+        </motion.h1>
       </div>
     </main>
   );
